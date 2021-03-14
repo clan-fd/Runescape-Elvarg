6bff8ce5b1cf96159fcd24bd45888f968a404b47
clan-fd <clan_fd@hotmail.com>
2021-03-13 19:42:51 -0800

    Initial files
    Uploading initial files to repository

---
+++ Client/src/module-info.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+/**
+ * @author Tau
+ *
+ */
+module ElvargClient {
+	exports com.runescape.collection;
+	exports com.runescape.cache.bzip;
+	exports com.runescape.draw.skillorbs;
+	exports com.runescape.net.requester;
+	exports com.runescape.model.content;
+	exports com.runescape.sign;
+	exports com.runescape.io.jaggrab;
+	exports com.runescape.cache.config;
+	exports com.runescape.cache.def;
+	exports com.runescape.io.packets.outgoing;
+	exports com.runescape.io;
+	exports com.runescape.draw;
+	exports com.runescape.sound;
+	exports com.runescape.util;
+	exports com.runescape.model;
+	exports com.runescape.entity.model;
+	exports com.runescape.cache;
+	exports com.runescape.scene.object.tile;
+	exports com.runescape.net;
+	exports org.seven.scene.graphic;
+	exports org.seven.cache.graphics;
+	exports com.runescape.io.packets.outgoing.impl;
+	exports org.seven.util;
+	exports com.runescape;
+	exports com.runescape.scene;
+	exports com.runescape.cache.anim;
+	exports com.runescape.cache.graphics;
+	exports com.runescape.scene.object;
+	exports com.runescape.entity;
+
+	requires java.desktop;
+	requires java.logging;
+}
